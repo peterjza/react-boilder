e72bfe0e5813297960cec15bfae72e1b8f574935
PeterJarvis <peter.jarvis@derivco.co.za>
2023-11-03 15:40:20 +0200

    scss support

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './app.scss';
 
 const root = createRoot(document.getElementById('root')!);
 
 const App: React.FC = () => {
-  return <h1>Helloo, React!</h1>;
+  return <h1 className='bg'>Helloo, React!</h1>;
 };
 
 
